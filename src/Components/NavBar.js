78c19694904a6fd5e1e758fa63cf05d27a6bf7de
jeff ndungu <ndungujeff.ke@gmail.com>
2023-01-23 14:43:01 +0300

    Done

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -1,18 +1,10 @@
 import React, {useState} from "react";
 
-function Item({ name, category }) {
+function Item({ name, category }) 
   const [addToCart, setAddToCart]= useState(true)
   const cartState = addToCart? "": "in-cart"
   const cartName = addToCart? "Add to Cart": "Remove From Cart"
 
-
-  return (
-    <li className={cartState}>
-      <span>{name}</span>
-      <span className="category">{category}</span>
-      <button className="add" onClick={changeCart}>{cartName}</button>
-    </li>
-  );
-}
+  
 
 export default Item;
